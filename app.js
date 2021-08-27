45b93c0db72f0446e5882d3135b4d0a6c277fcce
fryfritter <ryan.devqr@gmail.com>
2021-08-27 11:05:45 +0800

    add db connection for the main app

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const db = require("./db/models/index");
 
 app.use(express.json());
 
-// default error handler
+// sync will make sure the that the database is connected and the models are properly setup on app startup
+db.sequelize.sync();
+
+app.use(express.json());
+
 app.use((err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   res.status(err.statusCode).send(err.message);
