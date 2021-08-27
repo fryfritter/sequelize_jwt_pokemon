d496b3c1a277993fdb98bcfa40ea5f8edc990062
fryfritter <ryan.devqr@gmail.com>
2021-08-27 14:35:59 +0800

    add router for trainer

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require("express");
-const app = express();
 const db = require("./db/models/index");
+const cookieParser = require("cookie-parser");
 
 const pokemonRouter = require("./router/pokemons.router.js");
-app.use(express.json());
+const trainerRouter = require("./router/trainer.router.js");
 
-// sync will make sure the that the database is connected and the models are properly setup on app startup
 db.sequelize.sync();
 
+// sync will make sure the that the database is connected and the models are properly setup on app startup
+const app = express();
+app.use(cookieParser());
 app.use(express.json());
+
 app.use("/pokemon", pokemonRouter);
 
+app.use("/trainer", trainerRouter);
 app.use((err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   res.status(err.statusCode).send(err.message);
