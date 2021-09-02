3c99f9f537a4e781ceec4e38b7bc97ea897d4e94
fryfritter <ryan.devqr@gmail.com>
2021-09-02 15:50:45 +0800

    add front end app

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const cookieParser = require("cookie-parser");
 const pokemonRouter = require("./router/pokemons.router.js");
 const trainerRouter = require("./router/trainer.router.js");
 
+const path = require("path");
+const apiRouter = express.Router();
+
 db.sequelize.sync();
 
 // sync will make sure the that the database is connected and the models are properly setup on app startup
@@ -13,9 +16,17 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 
-app.use("/pokemon", pokemonRouter);
+app.use("/api", apiRouter);
+apiRouter.use("/pokemon", pokemonRouter);
+apiRouter.use("/trainer", trainerRouter);
+// app.use("/pokemon", pokemonRouter);
+// app.use("/trainer", trainerRouter);
 
-app.use("/trainer", trainerRouter);
+app.use(express.static(path.resolve("client", "build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve("client", "build", "index.html"))
+);
+
 app.use((err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   res.status(err.statusCode).send(err.message);
