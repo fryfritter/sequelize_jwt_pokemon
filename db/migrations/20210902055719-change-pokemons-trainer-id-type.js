e02059631dcf11d11316619c9936bebffffa3312
fryfritter <ryan.devqr@gmail.com>
2021-09-02 14:34:39 +0800

    allow casting of String to Integer

--- db/migrations/20210902055719-change-pokemons-trainer-id-type.js
+++ db/migrations/20210902055719-change-pokemons-trainer-id-type.js
@@ -3,10 +3,9 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.changeColumn("Pokemons", "trainerId", {
-      type: Sequelize.INTEGER,
+      type: 'INTEGER USING CAST("trainerId" as INTEGER)',
     });
   },
-
   down: async (queryInterface, Sequelize) => {
     await queryInterface.changeColumn("Pokemons", "trainerId", {
       type: Sequelize.STRING,
